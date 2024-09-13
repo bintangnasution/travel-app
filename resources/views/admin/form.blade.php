4a5e2ee5d41c67a2ae9b03c1194e8a5f2f9397d5
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-09-13 16:18:52 +0700

    fixx config tailwind

---
+++ resources/views/admin/form.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <p class="text-sm">halaman form</p>
+</body>
+</html>
