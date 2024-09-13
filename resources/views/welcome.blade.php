744a68e5209ac9de9eadfcd20d3ddbba47a91ef8
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-09-13 15:17:16 +0700

    setup tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-<p>landing-page-here</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel</title>
+  @vite('resources/css/app.css')
+</head>
+<body class="antialiased">
+  <p class="text-white bg-blue-500 border-yellow-900 border-2">Test Tailwind</p>
+</body>
+</html>
